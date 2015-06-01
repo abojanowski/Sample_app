42f072a8c984288ccabd481f0d46227802bb3e52
Arek Bojanowski <abojanowski>
2015-06-01 10:54:28 +0200

    I change email addresses unique Test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -39,16 +39,32 @@ class UserTest < ActiveSupport::TestCase
   		assert @user.valid?, "#{valid_adress.inspect} should be valid"
   	end			
   end
+  
+  test "email validation sholud reject invalid addresses" do
+    invalid_addresses = %w[user@example,com user_at_foo.org 
+                          user.name@example. foo@bar_baz.com foo@bar+baz.com
+                          foo@bars..com]
+    invalid_addresses.each do |invalid_address|
+      @user.email = invalid_address
+      assert_not @user.valid?
+    end
+  end
 
-  test "email addresses should be unique" do
-  	duplicate_user = @user.dup
-  	duplicate_user.email = @user.email.upcase
-  	@user.save
-  	assert_not duplicate_user.valid?
+  # test "email addresses should be unique" do
+  # 	duplicate_user = @user.dup
+  # 	duplicate_user.email = @user.email.upcase
+  # 	@user.save
+  # 	assert_not duplicate_user.valid?
+  # end
+
+  test "email adresses should be saved as lower-case" do
+    mixed_case_email = "Foo@ExAMPle.CoM"
+    @user.email = mixed_case_email
+    @user.save  
+    assert_equal mixed_case_email.downcase, @user.reload.email
   end
 
   test "password should have a minimum 6 characters" do
     @user.password = @user.password_confirmation = "a" * 5
-    # assert_not 
   end  
 end
